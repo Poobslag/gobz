6fb064d484f834bb16910a5ac918d0c2f38cc0ed
Poobslag <aaron.pieper@gmail.com>
2026-08-07 10:10:20 -0400

    Vague army algorithm scales better
    Looks at 15 units or 50%, whichever is smaller. This way small armies
    are still misrepresented, but an army with 1,000 units will still have a
    chance of bad information too.

--- project/src/main/data/dungeon.gd
+++ project/src/main/data/dungeon.gd
@@ -5,12 +5,17 @@ var army: Army
 
 func get_vague_army() -> Army:
 	var vague_army: Army = Army.new()
-	for i in range(0, army.items.size(), 2):
+	var vague_count: int = mini(ceili(army.items.size() / 2.0), 15)
+	var vague_scalar: float = army.items.size() / float(vague_count)
+	for i in vague_count:
 		var item: Army.ArmyItem = army.items[i]
 		var vague_item: Army.ArmyItem = item.duplicate()
-		if i != army.items.size() - 1:
-			vague_item.count *= 2
+		var scalar: int = int(vague_scalar)
+		if randf() < (vague_scalar - int(vague_scalar)):
+			scalar += 1
+		vague_item.count = Utils.big_mult(vague_item.count, scalar)
 		vague_army.add_item(vague_item)
+	
 	return vague_army
 
 
