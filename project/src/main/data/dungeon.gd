c7ddc106376bd107f66e199127e1fadcb1da5032
Poobslag <aaron.pieper@gmail.com>
2026-08-13 19:11:01 -0400

    Added perform_recon(force) flag.
    There were some bugs where recon was consistently reporting inflated
    numbers, but they seem to have been fixed.

--- project/src/main/data/dungeon.gd
+++ project/src/main/data/dungeon.gd
@@ -4,10 +4,11 @@ var name: String
 var army: Army = Army.new()
 var recon_army: Army = Army.new()
 
-func perform_recon() -> void:
-	if not recon_army.is_empty():
+func perform_recon(force: bool = false) -> void:
+	if not recon_army.is_empty() and not force:
 		return
 	
+	recon_army.reset()
 	var recon_count: int = mini(ceili(army.hordes.size() / 2.0), 15)
 	var recon_scalar: float = army.hordes.size() / float(recon_count)
 	for i in recon_count:
