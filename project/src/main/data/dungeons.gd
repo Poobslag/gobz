2f783730687ff80883fa4a77248b1eab8428e819
Poobslag <aaron.pieper@gmail.com>
2026-09-18 14:50:13 -0400

    Refactoring: Split out ArmyBar
    I experimented with giving the player's units a unit composition bar,
    but it's a little messy. The player's units are drawn in a big
    RichTextLabel and even having split out ArmyBar, giving one line in the
    RichTextLabel an outline is impossible -- so this would be a much larger
    refactoring.

--- project/src/main/data/dungeons.gd
+++ project/src/main/data/dungeons.gd
@@ -1,29 +1,36 @@
 class_name Dungeons
 
 static func get_dungeon_select_info(dungeon: Dungeon) -> Dictionary[String, Variant]:
-	var goblins_by_type: Dictionary[Gobs.Type, Big]
-	for type: Gobs.Type in Gobs.Type.values():
-		goblins_by_type[type] = Big.ZERO
-	for gob: Gob in dungeon.recon_army.gobs:
-		goblins_by_type[gob.type] = Big.add(goblins_by_type[gob.type], gob.get_count())
-	var type_summaries: Array[Dictionary] = []
-	for type: Gobs.Type in Gobs.Type.values():
-		if goblins_by_type[type] == Big.ZERO:
-			continue
-		type_summaries.append({
-			"type": type,
-			"goblins": goblins_by_type[type],
-		} as Dictionary[String, Variant])
-	type_summaries.sort_custom(func(a: Dictionary[String, Variant], b: Dictionary[String, Variant]) -> bool:
-		return a["goblins"].is_gt(b["goblins"])
-		)
-	
-	var attack_rating: String = Gobs.attack_rating(
-			dungeon.recon_army.get_total_attack().to_float() / dungeon.recon_army.get_total_goblins().to_float())
-	
-	var goblins_text: String = dungeon.recon_army.get_total_goblins().to_aa() + " " + attack_rating
-	
+	var type_summaries: Array[Dictionary] = get_type_summaries(dungeon.recon_army)
+	var goblins_text: String = get_goblins_text(dungeon.recon_army)
 	return {
 		"goblins_text": goblins_text,
 		"type_summaries": type_summaries,
 	}
+
+
+static func get_goblins_text(army: Army) -> String:
+	var attack_rating: String = Gobs.attack_rating(
+			army.get_total_attack().to_float() / army.get_total_goblins().to_float())
+	return army.get_total_goblins().to_aa() + " " + attack_rating
+
+
+static func get_type_summaries(army: Army) -> Array[Dictionary]:
+	var result: Array[Dictionary] = []
+	var goblins_by_type: Dictionary[Gobs.Type, Big]
+	for type: Gobs.Type in Gobs.Type.values():
+		goblins_by_type[type] = Big.ZERO
+	for gob: Gob in army.gobs:
+		goblins_by_type[gob.type] = Big.add(goblins_by_type[gob.type], gob.get_count())
+	
+	for type: Gobs.Type in Gobs.Type.values():
+		if goblins_by_type[type] == Big.ZERO:
+			continue
+		result.append({
+			"type": type,
+			"goblins": goblins_by_type[type],
+		} as Dictionary[String, Variant])
+	result.sort_custom(func(a: Dictionary[String, Variant], b: Dictionary[String, Variant]) -> bool:
+		return a["goblins"].is_gt(b["goblins"])
+		)
+	return result
--- project/src/main/home_base/dungeon_summary.gd
+++ project/src/main/home_base/dungeon_summary.gd
@@ -1,74 +1,21 @@
-@tool
 extends Control
 
 var dungeon: Dungeon:
 	set(value):
 		dungeon = value
-		_recalculate_layout()
+		_refresh()
 
-var _bar_widths: Array[float] = [50, 50, 50, 50, 50]
-var _bar_types: Array[Gobs.Type] = [Gobs.FIRE, Gobs.WATER, Gobs.GRASS, Gobs.ANGEL, Gobs.DEVIL]
+func _ready() -> void:
+	_refresh()
 
-func _recalculate_layout() -> void:
-	var dungeon_select_info: Dictionary[Variant, Variant] = Dungeons.get_dungeon_select_info(dungeon)
+
+func _refresh() -> void:
+	if dungeon == null:
+		return
 	
+	var goblins_text: String = Dungeons.get_goblins_text(dungeon.recon_army)
 	%Label.text = "%s - %s" % [
 			dungeon.name,
-			dungeon_select_info["goblins_text"],
+			goblins_text,
 		]
-	
-	var boss_dungeon: Dungeon = PlayerData.get_boss_dungeon()
-	
-	var boss_dungeon_goblins: float = boss_dungeon.recon_army.get_total_goblins().to_float()
-	var bar_width: float = remap(dungeon.recon_army.get_total_goblins().to_float(), 0, boss_dungeon_goblins, 100, 440)
-	
-	_bar_types.clear()
-	_bar_widths.clear()
-	for summary: Dictionary[Variant, Variant] in dungeon_select_info["type_summaries"]:
-		_bar_types.append(summary["type"])
-		_bar_widths.append( \
-				bar_width * summary["goblins"].to_float() / dungeon.recon_army.get_total_goblins().to_float())
-
-
-const FILL_COLORS: Dictionary[Gobs.Type, Color] = {
-	Gobs.FIRE: Color("b34947"),
-	Gobs.WATER: Color("49b3b1"),
-	Gobs.GRASS: Color("5bb362"),
-	Gobs.ANGEL: Color("ddebe8"),
-	Gobs.DEVIL: Color("744c8c"),
-}
-
-const LINE_COLORS: Dictionary[Gobs.Type, Color] = {
-	Gobs.FIRE: Color("6b304b"),
-	Gobs.WATER: Color("428ea1"),
-	Gobs.GRASS: Color("458a61"),
-	Gobs.ANGEL: Color("b4c9cc"),
-	Gobs.DEVIL: Color("493961"),
-}
-
-func _draw() -> void:
-	var bar_start: Vector2 = Vector2(0, 0)
-	for i: int in _bar_widths.size():
-		var bar_width: float = _bar_widths[i]
-		var fill_color: Color = FILL_COLORS[_bar_types[i]]
-		var line_color: Color = LINE_COLORS[_bar_types[i]]
-		var outline_rect: Rect2 = Rect2(bar_start, Vector2(bar_width, size.y))
-		
-		# shift bar below the text
-		outline_rect = outline_rect.grow_individual(0, -14, 0, 2)
-		
-		# draw dark outer rectangle
-		draw_rect(outline_rect, line_color)
-		
-		# draw light inner rectangle
-		var inner_rect: Rect2 = outline_rect.grow_individual(
-			-2 if i == 0 else 0, -2,
-			-2 if i == _bar_widths.size() -1 else 0, -2)
-		draw_rect(inner_rect, fill_color)
-		
-		# draw emoji
-		if bar_width > 18:
-			draw_string(Utils.EMOJI_FONT, outline_rect.get_center() + Vector2(-6, 4),
-					Gobs.emoji_from_type(_bar_types[i]), HORIZONTAL_ALIGNMENT_CENTER, -1, 12)
-		
-		bar_start.x += bar_width
+	%ArmyBar.army = dungeon.recon_army
